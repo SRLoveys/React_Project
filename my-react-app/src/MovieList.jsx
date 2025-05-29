294c846b1f12ab96ad97c01ddeb11ca8a2893972
SRLoveys <147052904+SRLoveys@users.noreply.github.com>
2025-05-29 17:43:59 -0230

    bootstrap

--- my-react-app/src/MovieList.jsx
+++ my-react-app/src/MovieList.jsx
@@ -2,15 +2,16 @@ import Movie from './movie.jsx'
 
 
 function MovieList({data}) {
-    
+
     return (
-        <>
+        <div className="bg-black text-white">
         {
             data.map( (movieList) => {
-                return <Movie name={movieList.name} year={movieList.year} rating={movieList.rating}/>
+                return <div className="bg-dark border-bottom border-secondary">
+                    <Movie name={movieList.name} year={movieList.year} rating={movieList.rating}/></div>
             })
         }
-        </>
+        </div>
     )
 }
 
--- my-react-app/src/NavBar.jsx
+++ my-react-app/src/NavBar.jsx
@@ -4,11 +4,11 @@ function NavBar() {
   const [status, setStatus] = useState(true);
 
   return(
-  <header>
-    <h1>
+  <header className="bg-black text-white p-3 justify-content-between align-items-center">
+    <h1 className="bg-dark border-bottom border-secondary">
       Assignment 1
     </h1>
-    <button onClick={() => setStatus(!status)}>
+    <button className="btn btn-light" onClick={() => setStatus(!status)}>
       {status ? "Login" : "Logout"}
     </button>
   </header>
